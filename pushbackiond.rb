3d18082eb797aa0d0ac8b455a4b6e2a9f99381c9
anelson <anelson@apocryph.org>
2008-10-04 19:43:26 +0000

    Initialize $io_death_count to zero

--- pushbackiond.rb
+++ pushbackiond.rb
@@ -1,5 +1,7 @@
 require 'rfuzz/pushbackio'
 
+$io_death_count = 0
+
 ### A specialization of PushBackIO without that bullshit random socket death crap
 class PushBackIoNd < RFuzz::PushBackIO
     def initialize(secondary)
