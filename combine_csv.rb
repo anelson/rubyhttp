5373eed00f2f589637fbf03e0524e63c70885d77
anelson <anelson@apocryph.org>
2008-10-05 03:52:54 +0000

    cleaned up results and generated a pretty graph

--- combine_csv.rb
+++ combine_csv.rb
@@ -2,9 +2,36 @@
 
 #Quick and dirty script to combine a bunch of .csv output files from rubyhttp into one, for
 #easy importing into Excel
+#
+# The end result is a csv file containing a table of values like so:
+#
+#         site1  site2   site3 site4 site5
+# libcurl 
+# rfuzz
+# stock
+# custom1
+# custom2
+#
+# where the table is filled out with %CPU usage values
 
-header = nil
-lines = []
+SITE_NAME_COLUMN = "Site"
+IMPL_NAME_COLUMN = "Impl"
+NET_HTTP_IMPL_NAME = "net/http"
+TOTAL_CPU_TIME_COLUMN = "Total CPU Time"
+CLOCK_TIME_COLUMN = "Clock Time"
+
+def avg(values)
+  return 0 if values == nil
+
+  total = 0
+  values.each { |val| total += val}
+
+  total / values.length
+end
+
+data = {}
+
+sites = []
 
 Dir.glob('*.csv') do |filename|
   #Parse components of the filename to get information that will be prepended to each
@@ -15,30 +42,71 @@ Dir.glob('*.csv') do |filename|
   platform = parts[1]
   test_variant = parts[2..-1].join('-')
 
-  line_prefix = [ruby_ver, platform, test_variant]
-
   File.open(filename, 'r') do |file|
     file_header = file.readline.chomp.split(',')
-    
-    #We assume all the files have the same header.  If this is the first file, save off its header, otherwise
-    #just drop it on the floor
-    header ||= file_header
+
     file.each_line do |line|
-      line = line_prefix + line.chomp.split(',')
-      lines << line
+      line = line.chomp.split(',')
+
+      # Create a hash where the keys are the column lables in file_header, and the values
+      # are the correponsing columns in line
+      line_data = {}
+      file_header.size.times do |i|
+        line_data[file_header[i]] = line[i]
+      end
+
+      #Determine which site this corresponds to
+      site = line_data[SITE_NAME_COLUMN]
+
+      #keep a unique list of sites
+      sites << site unless sites.include?(site)
+
+      #Compute the CPU percentage 
+      cpu_percentage = line_data[TOTAL_CPU_TIME_COLUMN].to_f / line_data[CLOCK_TIME_COLUMN].to_f
+
+
+      #Determine which HTTP impl this corresponds to
+      #if it's not net/http, use the name of the impl
+      #if it is net/http, use the name of the test variant
+      if line_data[IMPL_NAME_COLUMN] == NET_HTTP_IMPL_NAME
+        impl = test_variant
+      else
+        impl = line_data[IMPL_NAME_COLUMN]
+      end
+
+      # Build a string that combines the platform, ruby version, and impl name, which will key the
+      # data hash
+      key = "#{ruby_ver} #{platform} #{impl}"
+      data[key] ||= {}
+
+      #Within the data[key] hash, there's a key for each site
+      #The value for the key is an array of cpu percentage values
+      #for the net/http implementation there will only be one value, but
+      #since the other implementations get run once for every test varient, these
+      #other implementations will have multiple runs in one file, and thus multiple cpu percentage
+      #values.  Save them all, and we'll average them before output
+      data[key][site] ||= []
+      data[key][site] << cpu_percentage
     end
   end
 end
 
-#Put the fields extracted from the filename at the front of the header
-header = ["ruby_ver", "platform", "test_variant"] + header
 
-print header.join(',')
+#Output the results
+print "Impl Names/Sites"
+sites.each do |site|
+  print ","
+  print site
+end
 print "\n"
 
-lines.each do |line|
-  print line.join(',')
+data.each_pair do |impl, sites_data|
+  print impl
+  sites.each do |site|
+    cpu_percentages = sites_data[site]
+    print ","
+    print avg(cpu_percentages)
+  end
   print "\n"
 end
-
 
